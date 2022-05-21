cdcbecc92210d56bd0af8f9bc586f8ed73ae024c
Aldikitta <andi.kitta77@gmail.com>
2022-05-21 14:35:53 +0800

    Initial commit

---
+++ app/src/main/java/com/aldikitta/room_mad_aac/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.aldikitta.room_mad_aac
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
